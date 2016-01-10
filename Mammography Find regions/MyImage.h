63d17cbc504d94c66b3037554cb8e40fcfe30b63
Cassio M. M. Pereira <cassiomartini@gmail.com>
2016-01-09 22:50:12 -0200

    initial import

---
+++ Mammography Find regions/MyImage.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<iostream>
+
+namespace MyImage {
+	struct ImageInfo {
+		std::string imageBaseName;
+		std::string classType;
+		// cx and cy are the centers of the mass
+		int cx, cy, radius;
+	};
+}
+
+std::ostream& operator<<(std::ostream& os, const MyImage::ImageInfo &img)
+{
+	os << "ImageInfo: {" << img.imageBaseName << ", " << img.classType <<
+		", " << img.cx << ", " << img.cy << ", " << img.radius << "}";
+	return os;
+}
